593d407836b912f9d9e1a1e3087c838d9aaf20d7
yujinlim <adamlyj89@gmail.com>
2015-02-09 11:30:16 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,200 @@
+'use strict';
+
+var template = require('lodash.template');
+var isArray  = require('lodash.isarray');
+var debug    = require('debug')('gigaapi');
+var bluebird = require('bluebird');
+var request  = bluebird.promisify(require('request'));
+var api      = template('http://api.gigatools.com/<%= type %>.json');
+
+function _request(url, qs) {
+  return request({
+    uri: url,
+    json: true,
+    useQuerystring: true,
+    qs: qs
+  }).spread(function(res, body) {
+    debug(decodeURI(res.req.path));
+
+    if (!Array.isArray(body)) {
+      throw new Error('body is not an array but is a ' + typeof body);
+    }
+
+    if (body.length < 2 || body[1].length === 0) {
+      throw new Error('there is no data available for ' + decodeURI(res.req.path));
+    }
+
+    return body;
+  });
+}
+
+function returnEvents(body) {
+  return body[1];
+}
+
+module.exports = function(apiKey) {
+  return {
+    country: function(countries) {
+
+      var countryapi = api({
+        type: 'country'
+      });
+
+      debug(countryapi);
+
+      return _request(countryapi, {
+        'countries[]': countries,
+        'api_key': apiKey
+      }).then(returnEvents);
+    },
+    countryByDates: function(countries, startDate, endDate) {
+      var countryapi = api({
+        type: 'country'
+      });
+
+      debug(countryapi);
+
+      return _request(countryapi, {
+        'countries[]': countries,
+        'to_date[]': endDate,
+        'from_date[]': startDate,
+        'api_key': apiKey
+      }).then(returnEvents);
+    },
+    venue: function(venues) {
+      var venueApi = api({
+        type: 'venue'
+      });
+
+      debug(venueApi);
+
+      return _request(venueApi, {
+        'api_key': apiKey,
+        'venues[]': venues
+      }).then(returnEvents);
+    },
+    venueByDate: function(venues, startDate, endDate) {
+      var venueApi = api({
+        type: 'venue'
+      });
+
+      debug(venueApi);
+
+      return _request(venueApi, {
+        'api_key': apiKey,
+        'venues[]': venues,
+        'from_date[]': startDate,
+        'to_date[]': endDate
+      }).then(returnEvents);
+    },
+    city: function(cities) {
+      var cityApi = api({
+        type: 'city'
+      });
+
+      debug(cityApi);
+
+      return _request(cityApi, {
+        'api_key': apiKey,
+        'cities[]': cities
+      }).then(returnEvents);
+    },
+    cityByDates: function(cities, startDate, endDate) {
+      var cityApi = api({
+        type: 'city'
+      });
+
+      debug(cityApi);
+
+      return _request(cityApi, {
+        'api_key': apiKey,
+        'cities[]': cities,
+        'from_date[]': startDate,
+        'to_date[]': endDate
+      }).then(returnEvents);
+    },
+    all: function() {
+      var gigsApi = api({
+        type: 'gigs'
+      });
+
+      debug(gigsApi);
+
+      return _request(gigsApi, {
+        'api_key': apiKey
+      }).then(returnEvents);
+    },
+    dates: function(startDate, endDate) {
+      var gigsApi = api({
+        type: 'gigs'
+      });
+
+      debug(gigsApi);
+
+      return _request(gigsApi, {
+        'api_key': apiKey,
+        'from_date[]': startDate,
+        'to_date': endDate
+      }).then(returnEvents);
+    },
+    usersBySoundCloudIds: function(ids) {
+      var searchApi = api({
+        type: 'search'
+      });
+
+      debug(searchApi);
+
+      if (isArray(ids)) {
+        ids = ids.join(',');
+      }
+
+      return _request(searchApi, {
+        'api_key': apiKey,
+        'soundcloud_user_ids': ids
+      }).then(returnEvents);
+    },
+    getArtist: function(url) {
+      return _request(url)
+        .then(function(body) {
+          return body[0];
+        });
+    },
+    users: {
+      mixCloudUsername: function(username) {
+        var searchApi = api({
+          type: 'search'
+        });
+
+        debug(searchApi);
+        return _request(searchApi, {
+          'api_key': apiKey,
+          'mixcloud_username': username
+        });
+      },
+      twitter: function(username) {
+        var searchApi = api({
+          type: 'search'
+        });
+
+        debug(searchApi);
+
+        return _request(searchApi, {
+          'api_key': apiKey,
+          'twitter_username': username
+        });
+      },
+      soundcloud: function(username) {
+        var searchApi = api({
+          type: 'search'
+        });
+
+        debug(searchApi);
+
+        return _request(searchApi, {
+          'api_key': apiKey,
+          'soundcloud_username': username
+        });
+      }
+    }
+  };
+};
---
+++ test.js
@@ -0,0 +1,156 @@
+'use strict';
+
+var chai             = require('chai');
+var moment           = require('moment');
+var expect           = chai.expect;
+var apiKey           = process.env.GIGATOOL;
+var DATE_TIME_FORMAT = 'YYYY-MM-DD';
+
+
+describe('gigatool apis integration', function() {
+  this.timeout(20000);
+  var api;
+
+  before(function() {
+    api = require('./')(apiKey);
+  });
+
+  describe('get by country', function() {
+    var Promise;
+    before(function() {
+      Promise = api.country('Argentina');
+    });
+
+    it('should have events', function(done) {
+      Promise
+        .then(function(events) {
+          expect(events).to.have.length.above(0);
+          done();
+        });
+    });
+  });
+
+  describe('get by country with dates', function() {
+    var Promise;
+    before(function() {
+      var start = moment(['1990', '10', '10']).format(DATE_TIME_FORMAT);
+      var end = moment().format(DATE_TIME_FORMAT);
+
+      Promise = api.countryByDates('Argentina', 
+        start,
+        end);
+    });
+
+    it('should have events', function(done) {
+      Promise
+        .then(function(events) {
+          expect(events).to.have.length.above(0);
+          done();
+        });
+    });
+  });
+
+  describe('get by venue', function() {
+    var Promise;
+    
+    before(function() {
+      Promise = api.venue('cuba inc');
+    });
+
+    it('should have events', function(done) {
+      Promise
+        .then(function(events) {
+          expect(events).to.have.length.above(0);
+          done();
+        });
+    });
+  });
+
+  describe('get by venue with dates', function() {
+    var Promise;
+    before(function() {
+      var start = moment(['1990', '10', '10']).format(DATE_TIME_FORMAT);
+      var end = moment(['2020', '0', '1']).format(DATE_TIME_FORMAT);
+
+      Promise = api.venueByDate('cuba inc', 
+        start,
+        end);
+    });
+
+    it('should have events', function(done) {
+      Promise
+        .then(function(events) {
+          expect(events).to.have.length.above(0);
+          done();
+        });
+    });
+  });
+
+  describe('get by city', function() {
+    var Promise;
+    before(function() {
+      Promise = api.city('kuala lumpur');
+    });
+
+    it('should have events', function(done) {
+      Promise
+        .then(function(events) {
+          expect(events).to.have.length.above(0);
+          done();
+        });
+    });
+  });
+
+  describe('get by city with dates', function() {
+    var Promise;
+    before(function() {
+      var start = moment(['1990', '10', '10']).format(DATE_TIME_FORMAT);
+      var end = moment(['2020', '0', '1']).format(DATE_TIME_FORMAT);
+
+      Promise = api.cityByDates('kuala lumpur',
+        start, 
+        end);
+    });
+
+    it('should have events', function(done) {
+      Promise
+        .then(function(events) {
+          expect(events).to.have.length.above(0);
+          done();
+        });
+    });
+  });
+
+  describe('get all events from today', function() {
+    var Promise;
+    before(function() {
+      Promise = api.all();
+    });
+
+    it('should have events', function(done) {
+      Promise
+        .then(function(events) {
+          expect(events).to.have.length.above(0);
+          done();
+        });
+    });
+  });
+
+  describe('get events by dates', function() {
+    var Promise;
+    before(function() {
+      var start = moment().format(DATE_TIME_FORMAT);
+      var end = moment(['2020', '0', '1']).format(DATE_TIME_FORMAT);
+
+      Promise = api.dates(start, end);
+    });
+
+    it('should have events', function(done) {
+      Promise
+        .then(function(events) {
+          expect(events).to.have.length.above(0);
+          done();
+        });
+    });
+  });
+});
